d6ede5cca999af4148641c6eb04fb882848d517a
Cedric Durand <cedric.durand@ledger.fr>
2020-07-25 16:12:28 +0200

    removed magnetic sensor, added failsafe for i2c temp issues

--- betano.ino
+++ betano.ino
@@ -12,26 +12,9 @@
 // Accel/gyro address must be 0x6B, would be 0x6A if SDO_AG is LOW
 #define LSM9DS1_AG  0x6B
 
-#define NBALFALOOPS 2
-
 /* Communication with the outside world */
 BluetoothSerial ESP_BT;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-/* Chrono struct */
-struct Karen {
-    const uint8_t PIN; // Pin of the interrupt
-    uint32_t nRebounds; // number of time the interrupt was detected
-    uint32_t nTriggered; // number of sectors elapsed
-    bool triggered; // Set as true everytime the interrupt is detected, cleared when read as a sector
-    int64_t time;
-    float hotlap;
-    int64_t lap_minutes;
-    float lap_seconds;
-    char laptime[16];
-    char lapseconds[16];
-};
-Karen alfano = {12, 0, 0, false, 0, 0, 0, 0, ""};
-int64_t sectors[2048] = {0};
 
 /* Led indicator */
 String outputState = "off";
@@ -39,6 +22,10 @@ const int output = 13;
 
 /* IMU */
 LSM9DS1 imu;  // Create an LSM9DS1 object
+float tyre_temp= 0;
+float ambient_temp= 0;
+int temp_error= 0;
+
 // Global variables to keep track of update rates
 unsigned long startTime;
 unsigned int accelReadCounter = 0;
@@ -211,16 +198,6 @@ static void gaeta_timer_callback(void* arg)
   ESP_LOGI(TAG, "One-shot timer called, time since boot: %lld us", time_since_boot);
 }
 
-
-void IRAM_ATTR isr() {
-  alfano.nRebounds++;
-  if (alfano.triggered == false)
-  {
-    alfano.triggered = true;
-    alfano.nTriggered++;
-    alfano.time = esp_timer_get_time();
-  }
-}
 // printSensorReadings prints the latest IMU readings
 // along with a calculated update rate.
 void printSensorReadings()
@@ -284,10 +261,6 @@ void setup() {
   pinMode(output, OUTPUT);
   digitalWrite(output, HIGH);
 
-  // Attach the interrupt for the magnetic sensor
-  pinMode(alfano.PIN, INPUT_PULLUP);
-  attachInterrupt(alfano.PIN, isr, HIGH);
-
   // Start the timer
   esp_timer_create_args_t timer_args;
   timer_args.callback = &gaeta_timer_callback;
@@ -295,11 +268,6 @@ void setup() {
   esp_timer_handle_t gaeta;
   ESP_ERROR_CHECK(esp_timer_create(&timer_args, &gaeta));
   ESP_ERROR_CHECK(esp_timer_start_once(gaeta, 5000000));
-  sectors[0] = esp_timer_get_time();
-  for (int i = 1; i < NBALFALOOPS; i++) {
-    sectors[i] = sectors[0];
-    alfano.nTriggered++;
-  }
   round_i2c();
   Serial.println("Initializing the LSM9DS1");
   uint16_t status = initLSM9DS1();
@@ -338,23 +306,19 @@ void loop(){
   }
 
   int64_t current = esp_timer_get_time();
-  if (alfano.triggered && (current - alfano.time) > 1000000) {
-    sectors[alfano.nTriggered] = alfano.time;
-    alfano.hotlap =  (float)(sectors[alfano.nTriggered] - sectors[alfano.nTriggered - NBALFALOOPS]) / 1000000;
-    alfano.lap_minutes = alfano.hotlap / 60;
-    alfano.lap_seconds = fmod(alfano.hotlap, 60);
-
-    Serial.printf("Alfano has been pressed for the %u time. %u rebounds this time. "
-                  "Time of trigger %u; Sector %f seconds\nTime: %u:%09.6f [%u]\n",
-                  alfano.nTriggered, alfano.nRebounds,
-                  alfano.time, alfano.hotlap, alfano.lap_minutes, alfano.lap_seconds, NBALFALOOPS);
-    sprintf(alfano.laptime, "%u.%09.6f", alfano.lap_minutes, alfano.lap_seconds);
-    sprintf(alfano.lapseconds, "%09.6f", alfano.lap_seconds);
-    alfano.triggered = false;
-    alfano.nRebounds = 0;
-  }
   if (current - old_time > 100000) {
     //$RC3,[time],[count],[xacc],[yacc],[zacc],[gyrox],[gyroy],[gyroz],[rpm/d1],[d2],[a1],[a2],[a3],[a4],[a5],[a6],[a7],[a8],[a9],[a10],[a11],[a12],[a13],[a14],[a15]*[checksum]
+    float t_temp = mlx.readObjectTempC();
+    if (t_temp > 0 && t_temp < 150) {
+      ambient_temp = mlx.readAmbientTempC();
+      tyre_temp = t_temp;
+      temp_error = 0;
+    }
+    else
+    {
+      temp_error = 1;
+    }
+
     String str = String("$RC3,,") + String(count++) + ",";
     //str += String(tempTC,1) + ",";   //[a2],   (thermocouple)
     str+= String(imu.calcAccel(imu.ax)) + ",";
@@ -363,22 +327,21 @@ void loop(){
     str+= String(imu.calcGyro(imu.gx)) + ",";
     str+= String(imu.calcGyro(imu.gy)) + ",";
     str+= String(imu.calcGyro(imu.gz)) + ",,,"; // gyroz,d1,d2
-
-    str+= String(mlx.readObjectTempC()) + ","; // a1
-    str+= String(mlx.readAmbientTempC()) + ","; // a2
+    str+= String(tyre_temp) + ","; // a1
+    str+= String(ambient_temp) + ","; // a2
     str+= String(imu.calcMag(imu.mx)) + ","; // a3
     str+= String(imu.calcMag(imu.my)) + ","; // a4
     str+= String(imu.calcMag(imu.mz)) + ","; // a5
     str+= ","; // a6
     str+= ","; //a7
-    str+= String(alfano.nTriggered) + ","; //a8
-    str+= String((long)alfano.time) + ","; //a9
-    str+= + ","; //a10
-    str+= String(alfano.nRebounds) + ","; //a11
-    str+= String(alfano.hotlap) + ","; //a12
-    str+= String((long)alfano.lap_minutes) + ","; //a13
-    str+= String(alfano.lapseconds) + ","; //a14
-    str+= String(fmod(alfano.lap_seconds, 1) * 100) + "*"; //a15
+    str+= ","; //a8
+    str+= ","; //a9
+    str+= String(temp_error) + ","; //a10
+    str+= ","; //a11
+    str+= ","; //a12
+    str+= ","; //a13
+    str+= ","; //a14
+    str+= "*"; //a15
     Serial.println(str + checksum(str));
     ESP_BT.println(str + checksum(str));
 
